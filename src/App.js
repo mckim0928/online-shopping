152f952b4f27eac0cd935d52bd8274de690c10c3
am437 <am437@duke.edu>
2017-04-23 13:18:18 -0400

    added ad banners back in by importing images

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 import FluxCartApp from './components/FluxCartApp';
 
+import banner_0 from "../images/sale_banners/sale_banner_0.png";
+import banner_1 from "../images/sale_banners/sale_banner_1.png";
+import banner_2 from "../images/sale_banners/sale_banner_2.png";
+
 class App extends Component {
   render() {
     return (
@@ -18,7 +22,7 @@ class App extends Component {
                 <a className="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
                     All Departments <span className="caret"></span></a>
                 <ul className="dropdown-menu">
-                  <li><a href="./produce.html">Produce</a></li>
+                  <li><a href="../produce.html">Produce</a></li>
                   <li><a href="./frozen.html">Frozen</a></li>
                 </ul>
                 </li>
@@ -26,10 +30,10 @@ class App extends Component {
           </div>
 
           <div id="ads">
-            <img className="ad" src="~/images/sale_banners/sale_banner_0.png"
+            <img className="ad" src={banner_1}/>
+            <img className="ad" src={banner_0}
               alt="img not available"/>
-            <img className="ad" src="../images/sale_banners/sale_banner_1.png"/>
-            <img className="ad" src="../images/sale_banners/sale_banner_2.png"/>
+            <img className="ad" src={banner_2}/>
           </div>
           <FluxCartApp/>
         </div>
